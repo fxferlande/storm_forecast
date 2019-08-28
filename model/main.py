ad61f802a5a2a435e5295a9395b05b1892543212
François-Xavier Ferlande <fxferlande@quantmetry.com>
2019-08-28 13:52:54 +0000

    Ajout de la fonction pour sauvegarder le modèle en mémoire après entrainement

--- model/main.py
+++ model/main.py
@@ -91,6 +91,10 @@ def save_files():
              '/home/ubuntu/documents/storm_forecast/model/output/feature_extractor.py')
 
 
+def save_model(model, name='model'):
+    model.save('{}.h5'.format(name))
+
+
 def rmse(x, y):
     return np.sqrt(mean_squared_error(x, y))
 
@@ -132,6 +136,7 @@ if __name__ == "__main__":
 
     plot_model(output_path, model.cnn_model)
     save_files()
+    save_model(model.cnn_model)
     plot_history(output_path, history, do_cv)
     save_score(output_path, [rmse, r2_score], y_train,
                pred_train, y_test, pred_test, message=message)
