5002ff62f90226c64d58301731039c3bea528a8c
François-Xavier Ferlande <fxferlande@quantmetry.com>
2019-12-11 16:35:11 +0000

    Reformatage feature_extractor

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -8,11 +8,14 @@ class FeatureExtractor(object):
     def __init__(self, len_sequences=10):
         self.dummy_field = ["nature"]
         self.constant_fields = ['initial_max_wind', 'basin']
-        self.scalar_fields = ['instant_t', 'windspeed', 'latitude', 'longitude',
-                              'Jday_predictor', 'max_wind_change_12h', 'dist2land']
+        self.scalar_fields = ['instant_t', 'windspeed', 'latitude',
+                              'longitude', 'Jday_predictor',
+                              'max_wind_change_12h', 'dist2land']
         self.spatial_fields = ["u", "v", "sst", "slp", "hum", "z", "vo700"]
-        self.scaling_values = pd.DataFrame(index=self.spatial_fields + self.scalar_fields +
-                                           self.constant_fields, columns=["mean", "std"],
+        self.scaling_values = pd.DataFrame(index=self.spatial_fields +
+                                           self.scalar_fields +
+                                           self.constant_fields,
+                                           columns=["mean", "std"],
                                            dtype=float)
         self.binarizer = {}
         self.max_len = len_sequences
@@ -20,27 +23,23 @@ class FeatureExtractor(object):
     def make_sequence(self, X_df, field, normalize=True, padding_value=-100):
         ids = X_df.stormid.unique()
         f_data = X_df[[field, "stormid"]]
-        f_data[field] = (f_data[field] - self.scaling_values.loc[field, "mean"]) / \
+        f_data[field] = (f_data[field] -
+                         self.scaling_values.loc[field, "mean"]) / \
             self.scaling_values.loc[field, "std"]
         bloc = np.empty((len(X_df), self.max_len))
         for id in ids:
             ligne = f_data[f_data["stormid"] == id][field]
             seq_line = np.empty((len(ligne), self.max_len))
             for i in range(self.max_len):
-                seq_line[:, self.max_len - 1 -
-                         i] = np.array(([padding_value] * i + list(ligne))[0:len(ligne)])
+                seq_line[:, self.max_len - 1 - i] = \
+                    np.array(([padding_value] * i + list(ligne))[0:len(ligne)])
             pos = list(X_df["stormid"]).index(id)
             bloc[pos:pos + len(ligne), :] = seq_line
         return bloc
 
     def bearing(self, line):
-        """Compute bearing in degrees for one line of dataframe
-
-            brearing = atan2(
-                sin(Δlon).cos(lat2),
-                cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlon)
-            )
-            Source: https://www.movable-type.co.uk/scripts/latlong.html
+        """
+        Compute bearing in degrees for one line of dataframe
 
         Args:
             lon1, lat1, lon2, lat2: list-like coordinates in degrees
@@ -58,7 +57,8 @@ class FeatureExtractor(object):
         lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
         delta_lon = lon2 - lon1
         x = np.sin(delta_lon) * np.cos(lat2)
-        y = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(delta_lon)
+        y = np.cos(lat1) * np.sin(lat2) - \
+            np.sin(lat1) * np.cos(lat2) * np.cos(delta_lon)
 
         bearing = np.degrees(np.arctan2(x, y))
 
@@ -78,13 +78,16 @@ class FeatureExtractor(object):
         return X_df
 
     def cross_features(self, X_df):
-        X_df["mw12_imw"] = - X_df["max_wind_change_12h"]*X_df["initial_max_wind"]
+        X_df["mw12_imw"] = - X_df["max_wind_change_12h"] * \
+                            X_df["initial_max_wind"]
         if "mw12_imw" not in self.scalar_fields:
             self.scalar_fields.append("mw12_imw")
-        X_df["diff_current_max"] = X_df["windspeed"] - X_df["max_wind_change_12h"]
+        X_df["diff_current_max"] = X_df["windspeed"] - \
+            X_df["max_wind_change_12h"]
         if "diff_current_max" not in self.scalar_fields:
             self.scalar_fields.append("diff_current_max")
-        X_df["diff_current_init"] = X_df["windspeed"] - X_df["initial_max_wind"]
+        X_df["diff_current_init"] = X_df["windspeed"] - \
+            X_df["initial_max_wind"]
         if "diff_current_init" not in self.scalar_fields:
             self.scalar_fields.append("diff_current_init")
         return X_df
@@ -120,29 +123,33 @@ class FeatureExtractor(object):
         for field in self.spatial_fields:
             f_cols = X_df.columns[X_df.columns.str.contains(field + "_")]
             f_data = X_df[f_cols].values.reshape(-1, 11, 11)
-            field_grids.append(
-                (f_data - self.scaling_values.loc[field, "mean"]) / self.scaling_values.loc[field,
-                                                                                            "std"])
+            field_grids.append((f_data -
+                                self.scaling_values.loc[field, "mean"]) /
+                               self.scaling_values.loc[field, "std"])
             field_grids[-1][np.isnan(field_grids[-1])] = 0
             print("Field ", field, "just done")
         norm_image = np.stack(field_grids, axis=-1)
-        norm_image_final = np.reshape(norm_image, (len(X_df), len(self.spatial_fields)*11*11))
+        image_shape = (len(X_df), len(self.spatial_fields)*11*11)
+        norm_image_final = np.reshape(norm_image, image_shape)
 
         scalar = self.compute_bearing(X_df)
         scalar = self.cross_features(scalar)
         scalar = scalar[self.scalar_fields]
         scalar["stormid"] = X_df["stormid"]
-        final_scalar = np.empty((len(scalar), self.max_len, len(self.scalar_fields)))
+        final_scalar = np.empty((len(scalar), self.max_len,
+                                 len(self.scalar_fields)))
         for field in self.scalar_fields:
             bloc = self.make_sequence(scalar, field)
             final_scalar[:, :, self.scalar_fields.index(field)] = bloc
             print("Field ", field, "just done")
         norm_scalar = np.nan_to_num(final_scalar)
-        norm_scalar_final = np.reshape(norm_scalar, (len(X_df), self.max_len*len(self.scalar_fields)))
+        scalar_shape = self.max_len*len(self.scalar_fields)
+        norm_scalar_final = np.reshape(norm_scalar, (len(X_df), scalar_shape))
 
         norm_constant = X_df[self.constant_fields]
         for field in self.constant_fields:
-            norm_constant[field] = (norm_constant[field] - self.scaling_values.loc[field, "mean"]) / \
+            norm_constant[field] = (norm_constant[field] -
+                                    self.scaling_values.loc[field, "mean"]) / \
                 self.scaling_values.loc[field, "std"]
         norm_constant = norm_constant.values
         for field in self.dummy_field:
@@ -150,6 +157,8 @@ class FeatureExtractor(object):
             norm_constant = np.concatenate((norm_constant, dummy), axis=1)
         print("FeatureExtractor transform done")
         norm_constant_final = np.copy(norm_constant)
-        final = np.concatenate((norm_image_final, norm_scalar_final, norm_constant_final), axis=1)
 
+        final = np.concatenate((norm_image_final,
+                                norm_scalar_final,
+                                norm_constant_final), axis=1)
         return final
