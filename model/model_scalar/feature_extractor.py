675df18d4ae0e9b9c025251c6dd39935248abb73
François-Xavier Ferlande <fxferlande@quantmetry.com>
2020-09-30 13:33:45 +0000

    Separate model scalar

---
+++ model/model_scalar/feature_extractor.py
@@ -0,0 +1,240 @@
+import logging
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+pd.set_option('mode.chained_assignment', None)
+
+
+class FeatureExtractor(object):
+    def __init__(self, len_sequences: int = 10):
+        self.dummy_field = ["nature"]
+        self.constant_fields = ['initial_max_wind', 'basin']
+        self.scalar_fields = ['instant_t', 'windspeed', 'Jday_predictor',
+                              'max_wind_change_12h', 'dist2land']
+        self.spatial_fields = ["u", "v", "sst", "slp", "hum", "z", "vo700"]
+        self.scaling_values = pd.DataFrame(index=self.spatial_fields +
+                                           self.scalar_fields +
+                                           self.constant_fields,
+                                           columns=["mean", "std"],
+                                           dtype=float)
+        self.binarizer = {}
+        self.len_sequences = len_sequences
+
+    def make_sequence(self, X_df: pd.DataFrame, field: str,
+                      padding_value: int = -100) -> np.ndarray:
+        """
+        Selects the field out of the dataframe X_df, and computes sequences of
+        length self.len_sequences for each storm. The padding value is used for
+        the beginning of the sequence.
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+            field           (str):   name of the field to select
+            padding_value   (int):   value to fill at the begining of sequences
+
+        Returns:
+            np.ndarray:  Array of size (len(X_df), self.len_sequences)
+        """
+        ids = X_df.stormid.unique()
+        f_data = X_df[[field, "stormid"]]
+        f_data[field] = (f_data[field] -
+                         self.scaling_values.loc[field, "mean"]) / \
+            self.scaling_values.loc[field, "std"]
+        bloc = np.empty((len(X_df), self.len_sequences))
+        for id in ids:
+            ligne = f_data[f_data["stormid"] == id][field]
+            seq_line = np.empty((len(ligne), self.len_sequences))
+            for i in range(self.len_sequences):
+                seq_line[:, self.len_sequences - 1 - i] = \
+                    np.array(([padding_value] * i + list(ligne))[0:len(ligne)])
+            pos = list(X_df["stormid"]).index(id)
+            bloc[pos:pos + len(ligne), :] = seq_line
+        return bloc
+
+    def bearing(self, line: pd.Series) -> float:
+        """
+        Compute bearing in degrees for one line of dataframe
+
+        Args:
+            line   (pd.Series):  Row of dataframe with longitude, latitude,
+                                 longitude_before and latitude_before fields
+
+        Returns:
+            float: bearing in degrees
+        """
+        if line.instant_t == 0:
+            lon1, lat1 = line["longitude"], line["latitude"]
+            lon2, lat2 = line["longitude"], line["latitude"]
+        else:
+            lon1, lat1 = line["longitude"], line["latitude"]
+            lon2, lat2 = line["longitude_before"], line["latitude_before"]
+
+        lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+        delta_lon = lon2 - lon1
+        x = np.sin(delta_lon) * np.cos(lat2)
+        y = np.cos(lat1) * np.sin(lat2) - \
+            np.sin(lat1) * np.cos(lat2) * np.cos(delta_lon)
+
+        bearing = np.degrees(np.arctan2(x, y))
+
+        return bearing
+
+    def compute_bearing(self, X_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Compute bearing in degrees for all the lines of a dataframe
+
+        Args:
+            X_df    (pd.DataFrame): source DataFrame
+
+        Returns:
+            pd.DataFrame:   transformed dataframe
+        """
+        X_df["longitude_before"] = X_df["longitude"].shift()
+        X_df["latitude_before"] = X_df["latitude"].shift()
+        X_df["bearing"] = X_df.apply(self.bearing, axis=1)
+        X_df["bearing_cos"] = np.cos(X_df["bearing"])
+        X_df["bearing_sin"] = np.sin(X_df["bearing"])
+        X_df.drop("bearing", axis=1, inplace=True)
+        X_df.drop("longitude_before", axis=1, inplace=True)
+        X_df.drop("latitude_before", axis=1, inplace=True)
+        if "bearing_cos" not in self.scalar_fields:
+            self.scalar_fields += ["bearing_cos", "bearing_sin"]
+        return X_df
+
+    def cross_features(self, X_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Compute features based on other features (differences, multiplications)
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+
+        Returns:
+            pd.DataFrame:   transformed dataframe
+        """
+        X_df["mw12_imw"] = - X_df["max_wind_change_12h"] * \
+            X_df["initial_max_wind"]
+        if "mw12_imw" not in self.scalar_fields:
+            self.scalar_fields.append("mw12_imw")
+        X_df["diff_init_max"] = X_df["initial_max_wind"] - \
+            X_df["max_wind_change_12h"]
+        if "diff_init_max" not in self.scalar_fields:
+            self.scalar_fields.append("diff_init_max")
+        X_df["diff_current_init"] = X_df["windspeed"] - \
+            X_df["initial_max_wind"]
+        if "diff_current_init" not in self.scalar_fields:
+            self.scalar_fields.append("diff_current_init")
+        return X_df
+
+    def compute_aggregate_image(self, X_df) -> pd.DataFrame:
+        for field in self.spatial_fields:
+            f_cols = X_df.columns[X_df.columns.str.contains(field + "_")]
+            f_data = X_df[f_cols]
+            X_df = X_df.drop(f_cols, axis=1)
+            X_df[field] = np.nanmean(f_data, axis=1)
+            X_df[field].fillna(0, inplace=True)
+            if field not in self.scalar_fields:
+                self.scalar_fields.append(field)
+        return X_df
+
+    def fit(self, X_df: pd.DataFrame, y: np.ndarray = None) -> None:
+        """
+        Calculates mean and std values for each field. Image fields are
+        regrouped before calculation.
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+            y        (np.ndarray):   source target
+
+        Returns:
+            None
+        """
+        logging.info("Starting FeatureExtractor fit")
+        self.scaling_values.loc["windspeed", "mean"] = X_df["windspeed"].mean()
+        self.scaling_values.loc["windspeed", "std"] = X_df["windspeed"].std()
+
+        X_df = self.compute_bearing(X_df)
+        X_df = self.cross_features(X_df)
+        X_df = self.compute_aggregate_image(X_df)
+        for field in self.scalar_fields:
+            self.scaling_values.loc[field, "mean"] = X_df[field].mean()
+            self.scaling_values.loc[field, "std"] = X_df[field].std()
+        for field in self.constant_fields:
+            self.scaling_values.loc[field, "mean"] = X_df[field].mean()
+            self.scaling_values.loc[field, "std"] = X_df[field].std()
+        for field in self.dummy_field:
+            self.binarizer[field] = LabelBinarizer()
+            self.binarizer[field].fit(X_df[field])
+        logging.info("Fitting FeatureExtractor done")
+
+    def compute_scalar(self, X_df: pd.DataFrame) -> np.ndarray:
+        """
+        Processes scalar features, by making sequences and flattens them.
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+
+        Returns:
+            np.ndarray:   Array of size
+                        (len(X_df), self.len_sequences*len(self.scalar_fields))
+        """
+        scalar = self.compute_bearing(X_df)
+        scalar = self.cross_features(scalar)
+        scalar = self.compute_aggregate_image(scalar)
+        scalar = scalar[self.scalar_fields]
+        scalar["stormid"] = X_df["stormid"]
+        final_scalar = np.empty((len(scalar), self.len_sequences,
+                                 len(self.scalar_fields)))
+        for field in self.scalar_fields:
+            bloc = self.make_sequence(scalar, field)
+            final_scalar[:, :, self.scalar_fields.index(field)] = bloc
+        norm_scalar = np.nan_to_num(final_scalar)
+        scalar_shape = self.len_sequences*len(self.scalar_fields)
+        result = np.reshape(norm_scalar, (len(X_df), scalar_shape))
+        logging.info("FeatureExtractor: scalar transform done")
+        return result
+
+    def compute_constant(self, X_df: pd.DataFrame) -> np.ndarray:
+        """
+        Processes constant featuresreshapes them. It also binarizes dummy
+        fields.
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+
+        Returns:
+            np.ndarray:   Array of constant features
+        """
+        norm_constant = X_df[self.constant_fields]
+        for field in self.constant_fields:
+            norm_constant[field] = (norm_constant[field] -
+                                    self.scaling_values.loc[field, "mean"]) / \
+                self.scaling_values.loc[field, "std"]
+        norm_constant = norm_constant.values
+        for field in self.dummy_field:
+            dummy = self.binarizer[field].transform(X_df[field])
+            norm_constant = np.concatenate((norm_constant, dummy), axis=1)
+        result = np.copy(norm_constant)
+        logging.info("FeatureExtractor: constant transform done")
+        return result
+
+    def transform(self, X_df: pd.DataFrame) -> np.ndarray:
+        """
+        Processes all types of features and returns a flattened array. It is
+        a more standard format if we want to use libraries such as sklearn, but
+        the regressor will have to separate the different types of features in
+        the fit and predict
+
+        Args:
+            X_df   (pd.DataFrame):   source DataFrame
+
+        Returns:
+            np.ndarray:   Array of constant features
+        """
+        logging.info("Starting FeatureExtractor transform")
+        norm_scalar_final = self.compute_scalar(X_df)
+        norm_constant_final = self.compute_constant(X_df)
+
+        final = np.concatenate((norm_scalar_final,
+                                norm_constant_final), axis=1)
+        logging.info("FeatureExtractor transform done")
+        return final
---
+++ model/model_scalar/regressor.py
@@ -0,0 +1,258 @@
+import logging
+import time
+import numpy as np
+import pandas as pd
+import keras.backend as K
+from sklearn.base import BaseEstimator
+from keras.layers import Concatenate, Dropout, Activation, Dense, Input, \
+     Flatten, Conv1D, LSTM, Permute, RepeatVector, Multiply, Lambda
+from keras.models import Model
+from keras.regularizers import l2
+from keras.callbacks.callbacks import History
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+
+from model.scoring import rmse, weighted_rmse
+
+
+class Regressor(BaseEstimator):
+    def __init__(self, num_scalar=12, num_const=7, len_sequences=5):
+        self.epochs = 800
+        self.batch = 2000
+        self.len_lstm = 32
+        self.len_conv = 128
+        self.l2_weight = 4e-5
+        self.l2_lstm = 3e-5
+        self.l2_conv = 3e-2
+
+        self.lr = 0.00005
+
+        self.len_sequences = len_sequences
+        self.num_scalar = num_scalar
+        self.num_const = num_const
+
+        self.init_model()
+
+    def init_model(self, verbose: int = 1) -> None:
+        """
+        Creates the keras model for training. It is based on 4 blocks
+        corresponding to 4 sub-models:
+        - one processing the images
+        - one processing the scalar sequences through LSTM
+        - one processing the scalar sequences through CNN
+        - one processing the constant features
+        They are all concatenated at the end.
+
+        Args:
+            verbose   (int):   Option to show the parameters of the model once
+                               compiled
+
+        Returns:
+            None
+        """
+
+        scalar_in = Input(shape=(self.len_sequences, self.num_scalar))
+        const_in = Input(shape=(self.num_const,))
+
+        model_scalar = scalar_in
+        activations = LSTM(self.len_lstm, activation='relu',
+                           kernel_regularizer=l2(self.l2_lstm),
+                           return_sequences=True)(model_scalar)
+        attention = Dense(1, activation='relu')(activations)
+        attention = Flatten()(attention)
+        attention = Activation('softmax')(attention)
+        attention = RepeatVector(self.len_lstm)(attention)
+        attention = Permute([2, 1])(attention)
+
+        sent_representation = Multiply()([activations, attention])
+        sent_representation = Lambda(lambda xin: K.sum(xin, axis=-2),
+                                     output_shape=(self.len_lstm,))(
+                                     sent_representation)
+        model_scalar = Dense(256)(sent_representation)
+        model_scalar = Dropout(0.1)(model_scalar)
+        model_scalar = Dense(64)(model_scalar)
+        model_scalar = Dense(32)(model_scalar)
+        model_scalar = Activation("tanh")(model_scalar)
+
+        model_scalar_2 = Conv1D(self.len_conv, 3, padding="same",
+                                kernel_regularizer=l2(3e-5)
+                                )(scalar_in)
+        model_scalar_2 = Activation("selu")(model_scalar_2)
+        model_scalar_2 = Flatten()(model_scalar_2)
+        model_scalar_2 = Dense(32, kernel_regularizer=l2(self.l2_weight))(
+            model_scalar_2)
+        model_scalar_2 = Dense(16, kernel_regularizer=l2(self.l2_weight))(
+            model_scalar_2)
+        model_scalar_2 = Dropout(0.2)(model_scalar_2)
+        model_scalar_2 = Activation("tanh")(model_scalar_2)
+
+        model_const = const_in
+        model_const = Dense(64, kernel_regularizer=l2(self.l2_weight))(
+            model_const)
+        model_const = Activation("tanh")(model_const)
+
+        model = Concatenate()([model_scalar, model_scalar_2,
+                               model_const])
+
+        model = Dense(128, kernel_regularizer=l2(self.l2_weight))(model)
+        model = Dense(64, kernel_regularizer=l2(self.l2_weight))(model)
+        model = Activation("tanh")(model)
+        model = Dropout(0.2)(model)
+        model = Dense(32, kernel_regularizer=l2(self.l2_weight))(model)
+        model = Activation("tanh")(model)
+
+        model = Dense(1)(model)
+
+        self.model = Model([scalar_in, const_in], model)
+        self.model.compile(loss="mse", optimizer=Adam(learning_rate=self.lr))
+
+        if verbose == 1:
+            print(self.model.summary())
+        return
+
+    def fit(self, X: np.ndarray, y: np.ndarray, do_cv: bool = False,
+            verbose: int = 1) -> History:
+        """
+        Fits the model on X and y after extracting different subdatasets
+        (image, scalar, and constant) from X.
+
+        Args:
+            X   (np.ndarray):    Source array, containing all the type of
+                                 inputs, concatenated in one dataframe.
+            y    (np.ndarray):   Target
+            do_cv      (bool):   Option for keras integrated cross-validation
+            verbose     (int):   Verbose parameter for keras fit function
+
+        Returns:
+            History:  Keras history of training (useful for plotting curves)
+        """
+        t = time.time()
+        X = self.extract_subdatasets(X)
+
+        indexes = np.sum((X[1][:, :, -1] == -100)*1, axis=1)  \
+            <= 10
+        X = [x[indexes] for x in X]
+        x, _ = X
+
+        self.target_mean = y.mean()
+        self.target_std = y.std()
+        y = (y - self.target_mean)/self.target_std
+        y = y[indexes] - x[:, self.len_sequences-1, 1]
+        callback = EarlyStopping(monitor='val_loss', min_delta=0.01,
+                                 patience=100)
+        if do_cv:
+            history = self.model.fit(X, y, epochs=self.epochs,
+                                     batch_size=self.batch,
+                                     verbose=verbose,
+                                     validation_split=0.2,
+                                     callbacks=[callback])
+        else:
+            history = self.model.fit(X, y, epochs=self.epochs,
+                                     batch_size=self.batch,
+                                     verbose=verbose,
+                                     callbacks=[callback])
+        duration = int((time.time()-t)/60)
+        logging.info("Training done in {:.0f} mins".format(duration))
+        return history
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predicts output for X after extracting different subdatasets
+        (image, scalar, and constant).
+
+        Args:
+            X  (np.ndarray):    Source array, containing all the type of
+                                Inputs, concatenated in one dataframe.
+
+        Returns:
+            np.ndarray:  Predicted values for windspeed
+        """
+        X = self.extract_subdatasets(X)
+        x, _ = X
+        pred = self.model.predict(X).ravel() + \
+            x[:, self.len_sequences-1, 1]
+        pred = pred*self.target_std + self.target_mean
+        return pred
+
+    def extract_subdatasets(self, X: np.ndarray) -> list:
+        """
+        Extracts different subdatasets from X (image, scalar, and constant).
+        Based on the expected shapes, it splits X in three subdatasets and
+        reshapes them. Il allows us to give a DataFrame as input for the model.
+
+        Args:
+            X  (np.ndarray):    source array, containing all the type of
+                                inputs, concatenated in one dataframe.
+
+        Returns:
+            list:  List of the 3 subdatasets
+        """
+        break_scalar = self.len_sequences*self.num_scalar
+        break_const = break_scalar + self.num_const
+        num_samples = len(X)
+
+        shape_scalar = (num_samples, self.len_sequences, self.num_scalar)
+
+        norm_scalar = np.reshape(X[:, :break_scalar], shape_scalar)
+        norm_const = X[:, break_scalar: break_const]
+
+        return [norm_scalar, norm_const]
+
+    def score(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Computes rmse between target y and predictions for input X after
+        extracting different subdatasets (image, scalar, and constant).
+
+        Args:
+            X  (np.ndarray):     Source array, containing all the type of
+                                 inputs, concatenated in one dataframe.
+            y    (np.ndarray):   Target
+
+        Returns:
+            float:  rmse of predictions
+        """
+        X = self.extract_subdatasets(X)
+        pred = self.model.predict(X)
+        return rmse(pred, y)
+
+    def compute_scores(self, X: np.ndarray, y: np.ndarray,
+                       name: str = "") -> pd.Series:
+        scores = pd.Series()
+        pred = self.predict(X)
+        X_array = self.extract_subdatasets(X)
+        len_sequences = np.sum(((X_array[1] > -10)*1)[:, :, 1], axis=1)
+
+        scores.loc["RMSE{}".format(name)] = rmse(pred, y)
+
+        weight = len_sequences/max(len_sequences)
+        scores.loc["cust_rmse{}".format(name)] = weighted_rmse(pred, y, weight)
+
+        return scores
+
+    def get_params(self, deep: bool = True) -> dict:
+        """
+        Returns the parameters of the model.
+
+        Args:
+            deep  (bool):  True
+
+        Returns:
+            dict:   Parameters of the model
+        """
+        return {"epochs": self.epochs,
+                "batch": self.batch,
+                "len_sequences": self.len_sequences,
+                "num_scalar": self.num_scalar,
+                "num_const": self.num_const}
+
+    def set_params(self, **parameters) -> None:
+        """
+        Sets the parameters of the model.
+
+        Returns:
+            None
+        """
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        self.init_model(verbose=0)
+        return self
